21ac6c0d73904e8ea8714160b3656f6cf1fb204b
danielghandahari <dangha94@gmail.com>
2018-01-23 12:10:59 +0100

    connect react app to git

---
+++ server.js
@@ -0,0 +1,41 @@
+/**
+ * Created by danielghandahari on 2018-01-23.
+ */
+const {createServer} = require('http');
+const express = require('express');
+const compression = require('compression');
+const morgan = require('morgan');
+const path = require('path');
+
+
+const normalizePort = port => parseInt(port, 10);
+const PORT = normalizePort(process.env.PORT || 5000);
+
+const app = express();
+const dev = app.get('env') !== 'production';
+
+if(!dev) {
+    app.disable('x-powered-by');
+    app.use(compression());
+    app.use(morgan('common'));
+
+    app.use(express.static(path.resolve(__dirname, 'dist')));
+
+    app.get('/', (req, res) => {
+        res.send("HEEEEJ")
+    });
+
+}
+
+if(dev) {
+    app.use(morgan('dev'));
+
+}
+
+const server = createServer(app);
+
+server.listen(PORT, err => {
+    if(err) throw err;
+
+    console.log('Server started!');
+});
