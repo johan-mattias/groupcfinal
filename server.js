b963c5ec394666bf8d5948a2967719a24b041139
Tor Berglind <torberglind@nl119-179-121.student.uu.se>
2018-01-24 13:08:27 +0100

    installed mongoose & set URI connection

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const express = require('express');
 const compression = require('compression');
 const morgan = require('morgan');
 const path = require('path');
+var mongoose = require ("mongoose");
+
+//  type: heroku config:get MONGODB_URI
+//  You can add mLab to your app either through the add-on catalog or through the heroku command.
+//  https://devcenter.heroku.com/articles/mongolab
+mongoose.connect('mongodb://heroku_6dn4h73b:co8pe58sjmurt19g8t7ddd7vef@ds111618.mlab.com:11618/heroku_6dn4h73b')
 
 
 const normalizePort = port => parseInt(port, 10);
