6f954841aacf43818151f15f2919c64ffc062888
danielghandahari <dangha94@gmail.com>
2018-01-25 12:16:02 +0100

    push heroku changes to master branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import './scss/app.css'
+
+import { Home } from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <div>
         <p>This is our init page, main entry point.</p>
+        <Home/>
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+/**
+ * Created by danielghandahari on 2018-01-24.
+ */
+import React, { Component } from 'react';
+
+export class Home extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        }
+    }
+
+    render() {
+        return(
+            <div className="test"></div>
+        );
+    }
+}
