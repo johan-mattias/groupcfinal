5cd2180d9cb00ef104540a467cc3b3440e5889ff
danielghandahari <dangha94@gmail.com>
2018-01-26 21:56:52 +0100

    implemented search filter

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 /**
  * Created by danielghandahari on 2018-01-24.
+ *
+ *  Inspired by LevelUpTuts: https://www.youtube.com/channel/UCyU5wkjgQYGRB0hIHMwm2Sg
+ *
  */
 import React, { Component } from 'react';
 
@@ -9,14 +12,39 @@ export class Home extends Component {
         super(props);
 
         this.state = {
-            items: []
+            searchField: ''
         }
     }
 
+    updateField(e) {
+        this.setState({
+            searchField: e.target.value.substr(0, 20)
+        });
+    }
+
     render() {
+        let dummyList = [{name: "Coca-cola"}, {name: "Haribo"}, {name: "Pepsi"}, {name: "Hockey Pulver"}, {name: "Kaffe"}
+            , {name: "Nocco"}, {name: "Powerking"}, {name: "Delicatobollar"}, {name: "Kebab"}, {name: "Baklava"}, {name: "Fanta"}
+            , {name: "Sprite"}, {name: "Surisar"}, {name: "Remmar"}, {name: "Pärlboll"}, {name: "IT-tröja"}, {name: "IT-kopp"}, {name: "Te"}];
+        let dummyListFiltered = dummyList.filter(
+            (elem) => {
+                return elem.name.toLowerCase().indexOf(
+                    this.state.searchField.toLowerCase()) !== -1;
+            }
+        );
         return(
             <div>
-                <input className="search-bar" type="search"/>
+                <ul>
+                    {
+                        dummyListFiltered.map((elem) => {
+                            return <li>{elem.name}</li>
+                        })
+                    }
+                </ul>
+                <input className="search-bar" type="text"
+                value={this.state.searchField}
+                onChange={this.updateField.bind(this)}
+                />
             </div>
         );
     }
