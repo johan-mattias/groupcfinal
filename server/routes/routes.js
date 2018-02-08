289c86d7d4c7b85cfb73928c3a4483ce4b156595
Tor Berglind <torberglind@nl107-186-35.student.uu.se>
2018-02-08 14:51:26 +0100

    Added route /delete-by-id

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,24 +1,13 @@
 //server/routes/routes.js
-//code inspired by https://blog.cloudboost.io/creating-your-first-mern-stack-application-b6604d12e4d3
+//code inspired by:
 //https://hackernoon.com/how-to-combine-a-nodejs-back-end-with-a-reactjs-front-end-app-ea9b24715032
 //https://blog.cloudboost.io/creating-your-first-mern-stack-application-b6604d12e4d3
 
 //====LIST DEPENDENCIES===//
 const express = require('express');
-const parseurl = require('parseurl');
-const bodyParser = require('body-parser');
-const path = require('path');
-const expressValidator = require('express-validator');
-const mongoose = require('mongoose');
 const Item = require('../../models/Item');
-const app = express();
 const router = express.Router();
-//const url = 'mongodb://heroku_6dn4h73b:co8pe58sjmurt19g8t7ddd7vef@ds111618.mlab.com:11618/heroku_6dn4h73b';
-const url = 'mongodb://armanv:OVtDy6R9ZgnYI3qZ5IUZB@ds117158.mlab.com:17158/scrubit';
-
-
 //=========================//
-
 
 router.get('/hej', function(req, res){
     res.send("hejsannnn");
@@ -59,7 +48,6 @@ router.post('/update-price', function (req, res) {
     });
 });
 
-
 router.post('/delete', function(req, res){
     Item.find({
         description: req.body.Name
@@ -68,5 +56,15 @@ router.post('/delete', function(req, res){
     });
 });
 
+router.post('/delete-by-id', function(req, res){
+    Item.remove({ _id: req.body.id }, function(err) {
+        if (!err) {
+            console.log('Item removed');
+        }
+        else {
+            console.log('Error removing');
+        }
+    });
+});
 
 module.exports = router;
