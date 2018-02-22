a724cee288974b8fe9e7b8409fd6dea7f3871c7a
danielghandahari <dangha94@gmail.com>
2018-02-22 23:13:14 +0100

    added swish link

--- server/server.js
+++ server/server.js
@@ -67,6 +67,13 @@ if(!dev) {
 if(dev) {
     app.use(morgan('dev'));
 
+    app.use(express.static(path.resolve(__dirname, '../build')));
+
+
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+    })
 }
 
 const server = createServer(app);
--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -9,7 +9,7 @@ export class StatusBar extends Component {
         return(
             <div className="status-flex">
                 <div className="sum-status">{this.props.totalSum} kr</div>
-                <button className="pay-btn">Pay</button>
+                <a href="swish://payment?data=%7B%22version%22%3A1%2C%22payee%22%3A%7B%22value%22%3A%20%22%2B46703382240%22%7D%2C%22amount%22%3A%7B%22value%22%3A200%7D%2C%22message%22%3A%7B%22value%22%3A%22ChristianAaaaaaalex%22%7D%7D&callbackurl=plick%3A%2F%2F" className="pay-btn">Pay</a>
             </div>
         )
     }
