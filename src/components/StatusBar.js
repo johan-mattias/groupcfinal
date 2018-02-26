b31c41b67b03bf76aed0993b4010229c380fff93
danielghandahari <dangha94@gmail.com>
2018-02-26 19:07:54 +0100

    changing params

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -6,8 +6,7 @@ import React, { Component } from 'react'
 export class StatusBar extends Component {
 
     render() {
-        let message = 'skrubben.setIsGrateful(true)'
-        let canSwish = this.props.totalSum > 0 ? 'swish://payment?data=%7B%22version%22%3A1%2C%22payee%22%3A%7B%22value%22%3A%20%221236130983%22%7D%2C%22amount%22%3A%7B%22' + '42' + '%22%3A200%7D%2C%22message%22%3A%7B%22value%22%3A%22' + message + '%22%7D%7D&callbackurl=plick%3A%2F%2F' : ''
+        let canSwish = this.props.totalSum > 0 ? 'swish://payment?data=%7B%22version%22%3A1%2C%22payee%22%3A%7B%22value%22%3A%20%22%2B46703382240%22%7D%2C%22amount%22%3A%7B%22value%22%3A200%7D%2C%22message%22%3A%7B%22value%22%3A%22skrubben.setIsGrateful%28true%29%22%7D%7D&callbackurl=plick%3A%2F%2F' : ''
 
         return(
             <div className="status-flex">
