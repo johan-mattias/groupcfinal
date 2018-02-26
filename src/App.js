ffe627190639f5024c1e74f2ff5094c9a7d10c75
danielghandahari <dangha94@gmail.com>
2018-02-26 18:50:42 +0100

    implemented Swish

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './scss/app.css'
 import { Home } from './components/Home';
 
 class App extends Component {
+
   render() {
     return (
       <div>
--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -6,10 +6,13 @@ import React, { Component } from 'react'
 export class StatusBar extends Component {
 
     render() {
+        let message = "skrubben.setIsGrateful(true)"
+        let canSwish = this.props.totalSum > 0 ? 'swish://payment?data=%7B%22version%22%3A1%2C%22payee%22%3A%7B%22value%22%3A%20%221236130983%22%7D%2C%22amount%22%3A%7B%22' + this.props.totalSum + '%22%3A200%7D%2C%22message%22%3A%7B%22value%22%3A%22' + message + '%22%7D%7D&callbackurl=plick%3A%2F%2F' : ''
+
         return(
             <div className="status-flex">
                 <div className="sum-status">{this.props.totalSum} kr</div>
-                <a href="swish://payment?data=%7B%22version%22%3A1%2C%22payee%22%3A%7B%22value%22%3A%20%22%2B46703382240%22%7D%2C%22amount%22%3A%7B%22value%22%3A200%7D%2C%22message%22%3A%7B%22value%22%3A%22ChristianAaaaaaalex%22%7D%7D&callbackurl=plick%3A%2F%2F" className="pay-btn">Pay</a>
+                <a href={canSwish} className="pay-btn">Pay</a>
             </div>
         )
     }
