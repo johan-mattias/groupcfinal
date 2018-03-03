e36d8f7519e44f34d11907aa9a9805ce5c32cd8e
danielghandahari <dangha94@gmail.com>
2018-03-03 18:04:12 +0100

    flexbox wrapping on items and styling + hiding status bar when tot == 0 + navbar styling with logo + box-shadow for iPhone fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@
 import React, { Component } from 'react'
 import { StatusBar } from './StatusBar'
 import axios from 'axios'
-import scrubitLogo from '../images/scrubitLog.png'
+import scrubitLogo from '../images/ScrubIT-Logo-Black.png'
 
 
 export class Home extends Component {
@@ -22,7 +22,8 @@ export class Home extends Component {
                 , {id: 5, name: "Nocco", price: 33}, {id: 6, name: "Powerking", price: 1337}, {id: 7, name: "Delicatobollar", price: 10}, {id: 8, name: "Kebab", price: 70}, {id: 9, name: "Baklava", price: 50}, {id: 10, name: "Fanta", price: 7}
                 , {id: 11, name: "Sprite", price: 7}, {id: 12, name: "Surisar", price: 2}, {id: 13, name: "Remmar", price: 1}, {id: 14, name: "Pärlboll", price: 10}, {id: 15, name: "IT-tröja", price: 100}, {id: 16, name: "IT-kopp", price: 100}, {id: 17, name: "Te", price: 100}],
             itemQuant: [],
-            items: []
+            items: [],
+            barIsHidden: true
         }
 
     }
@@ -30,7 +31,7 @@ export class Home extends Component {
 
     componentDidMount()
     {
-        axios.get('http://scrubit.herokuapp.com/api/get-all')
+        axios.get('/api/get-all')
         .then((response) => {
 
 
@@ -72,14 +73,29 @@ export class Home extends Component {
         const newItemQuantites = this.state.itemQuant
         newItemQuantites[elem.indexId]--
 
+        const newTotSum = this.state.totalSum - elem.price
+
         this.setState({
             itemQuant: newItemQuantites,
-            totalSum: this.state.totalSum - elem.price
+            totalSum: newTotSum
         });
+
+        console.log(newTotSum)
+        if(newTotSum === 0) {
+            this.setState({
+                barIsHidden: true
+            });
+        }
     }
 
     onPlus = (event, elem) => {
         event.preventDefault()
+
+        if(this.state.totalSum === 0) {
+            this.setState({
+                barIsHidden: false
+            });
+        }
 
         const newItemQuantites = this.state.itemQuant
         newItemQuantites[elem.indexId]++
@@ -105,14 +121,15 @@ export class Home extends Component {
 
 
         return(
+
             <div>
                 <div className="top-container">
-                    {/*<img id="main-logo" src={scrubitLogo} alt=""/>*/}
                     <input className="search-bar" type="text"
                            value={this.state.searchField}
                            onChange={this.updateField.bind(this)}
-                           placeholder={"e.g. Coca-cola, Haribo, Binary search tree"}
+                           placeholder={"e.g. Coca-cola, Haribo, Binary search tree..."}
                     />
+                    <img id="main-logo" src={scrubitLogo} alt=""/>
                 </div>
 
 
@@ -120,13 +137,14 @@ export class Home extends Component {
 
                     {
                         itemsFiltered.map((elem) => {
-                            return <div className="items-flex">
+                            return <div key={elem.indexId} className="items-flex">
 
                                     <div className="items-flex-item item-info-container">
                                         {/*src='https://dummyimage.com/100x100/000/fff.png'*/}
                                         <img src={elem.url} alt=""/>
-                                        <li className="item-info">{elem.name + ": " + elem.price + "kr"}</li>
+                                        <li className="item-info">{elem.name}</li>
                                     </div>
+                                    <div className="items-flex-item item-price">{elem.price + ' kr'}</div>
                                     <div className="items-flex-item quantity-holder">
                                         <button onClick={(event) => { this.onMinus(event, elem)}}>-</button>
                                         <div>{this.state.itemQuant[elem.indexId]}</div>
@@ -137,7 +155,7 @@ export class Home extends Component {
                         })
                     }
                 </div>
-                <StatusBar totalSum={this.state.totalSum}/>
+                <StatusBar isHidden={this.state.barIsHidden} totalSum={this.state.totalSum}/>
             </div>
 
         );
--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -4,18 +4,27 @@
 import React, { Component } from 'react'
 
 export class StatusBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hidden: props.totalSum === 0 ? 'hidden' : 'NO'
+        }
+    }
 
     render() {
         let totalToStr = this.props.totalSum.toString()
         let canSwish = this.props.totalSum > 0 ? 'swish://payment?data=%7B%22version%22%3A1%2C%22payee%22%3A%7B%22value%22%3A%20%22%2B46703382240%22%7D%2C%22amount%22%3A%7B%22value%22%3A' + totalToStr +'%7D%2C%22message%22%3A%7B%22value%22%3A%22skrubben.setIsGrateful%28true%29%22%7D%7D&callbackurl=plick%3A%2F%2F' : ''
 
+        let hiddenClass = this.props.totalSum === 0 ? 'hidden-bar' : 'shown-bar'
+
         return(
-            <div className="status-flex">
+            <div className={"status-flex " + hiddenClass}>
                 <div className="sum-status">
                     <div className="tot-title">
                         {this.props.totalSum}
                     </div>
-                    /*TODO: flytta funktionalitet from sum-status to tot-amount*/
+                    {/*TODO: flytta funktionalitet from sum-status to tot-amount*/}
                     <div className="tot-amount">
                         <div className="tot-amount">{this.props.totalSum}</div>
                         <div className="tot-sek">kr</div>
